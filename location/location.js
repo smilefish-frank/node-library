0406ef674a696c74f0588b12bb450f6f0d42e872
Frank Ayars <fayars@brandify.com>
2017-02-16 06:01:16 -0800

    location.js

--- location/location.js
+++ location/location.js
@@ -1,51 +1,49 @@
 //location library
 var config = require("config")
 
-var location = {};
-
-location.getLocation = function getLocation(locationId)
+module.exports = function getLocation(locationId)
 {
     //do actual library type work here
-    var l = new Object();
-    l.Stage = config.stage;
-    l.LocationId = locationId;
-    l.Name = "Location 1";
-    l.Address = "777 Somewhere, Corona, CA 92881 --- 1.0.2";
-    var json = JSON.stringify(l);
+    var location = new Object();
+    location.Stage = config.stage;
+    location.LocationId = locationId;
+    location.Name = "Location 1";
+    location.Address = "777 Somewhere, Corona, CA 92881 --- 1.0.2";
+    var json = JSON.stringify(location);
 
     return json;
 }
 
-location.getLocationByClientId = function getLocationByClientId(clientId)
+function getLocationByClientId(clientId)
 {
-    var l = new Object();
-    l.ClientId = clientId;
-    l.Name = "Location 1";
-    l.Address = "777 Somewhere, Corona, CA 92881"
-    var json = JSON.stringify(l);
+    var location = new Object();
+    location.ClientId = clientId;
+    location.Name = "Location 1";
+    location.Address = "777 Somewhere, Corona, CA 92881"
+    var json = JSON.stringify(location);
 
     return json;
 }
 
-location.getLocationsByGeoCode = function getLocationsByGeoCode(lat, lng)
+function getLocationsByGeoCode(lat, lng)
 {
     var locations = new Array();
-    var l = new Object();
-    l.Name = "Location 1";
-    l.Address = "777 Somewhere, Corona, CA 92881"
-    l.push(location);
+    var location = new Object();
+    location.Name = "Location 1";
+    location.Address = "777 Somewhere, Corona, CA 92881"
+    locations.push(location);
     
-    l = new Object();
-    l.Name = "Location 2";
-    l.Address = "888 Here, Corona, CA 92881";
-    l.push(location);
+    location = new Object();
+    location.Name = "Location 2";
+    location.Address = "888 Here, Corona, CA 92881";
+    location.push(location);
 
-    var json = JSON.stringify(l);
+    var json = JSON.stringify(location);
 
     return json;
 }
 
-location.getLocationsByAddress = function getLocationsByAddress(address)
+function getLocationsByAddress(address)
 {
     var locations = new Array();
     var location = new Object();
